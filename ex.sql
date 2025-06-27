77bee4fcddc3c3802123be9b1a6cf35ff21ecd81
ccpchin <chinmayrsharma71103@gmail.com>
2025-06-27 10:26:39 +0530

    Update ex.sql

--- ex.sql
+++ ex.sql
@@ -11,7 +11,9 @@ CREATE TABLE Class(
     Class_id integer unique PRIMARY KEY,
     Teacher Text not null
 );
-SELECT e.Student_Name, e.Student_Id, e.Class, c.teacher, c.class_id, e.marks
+SELECT e.Student_Name, e.Student_Id, e.Class, c.teacher, c.class_id, SUM(e.marks), AVG(e.Marks), COUNT(*)
 from Example e, Class c
 Where e.class_id=c.class_id or( marks between 50 and 70 and Student_Name like "R%")
+GROUP BY e.Student_id
+HAVING SUM(e.marks)<100 and AVG(e.marks)<100
 Order by c.class_id;
