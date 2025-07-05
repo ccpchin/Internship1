371b9cf87b2f9b62010c646a332929a36faf1bfc
ccpchin <chinmayrsharma71103@gmail.com>
2025-07-05 10:29:20 +0530

    Create proc.sql

---
+++ proc.sql
@@ -0,0 +1,9 @@
+DELIMITER $$$
+Create PROCEDURE proced (
+  in pr_sid int ,
+  in minmarks int )
+Begin
+Select Student_Name,Marks
+From Example
+Where Student_Id=pr_sid and Marks>=minmarks;
+End $$
